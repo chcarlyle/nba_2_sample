6ce0b5e1b5cf29de91e2942ef2ed5af599c223f8
Caleb Carlyle <caleb.h.carlyle@gmail.com>
2024-12-03 09:54:29 -0700

    Adding other necessary files

---
+++ tests/test_nba_2_sample.py
@@ -0,0 +1,31 @@
+import unittest
+import pandas as pd
+from NBA_2_SAMPLE.data_cleaning import clean_data
+from NBA_2_SAMPLE.analysis import calculate_margin
+
+class TestNBADataScraper(unittest.TestCase):
+
+    def test_clean_data(self):
+        # Sample raw data for testing
+        data = {
+            'date_game': ['2024-10-01', '2024-10-02'],
+            'vpts': ['100', '95'],
+            'hpts': ['105', '90'],
+        }
+        df = pd.DataFrame(data)
+        cleaned_df = clean_data(df)
+        self.assertEqual(cleaned_df.shape[0], 2)
+
+    def test_calculate_margin(self):
+        data = {
+            'hpts': [105, 90],
+            'vpts': [100, 95],
+        }
+        df = pd.DataFrame(data)
+        df_with_margin = calculate_margin(df)
+        self.assertTrue('margin' in df_with_margin.columns)
+        self.assertEqual(df_with_margin['margin'][0], 5)
+        self.assertEqual(df_with_margin['margin'][1], -5)
+
+if __name__ == '__main__':
+    unittest.main()
