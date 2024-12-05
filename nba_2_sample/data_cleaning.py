6667a00e18483d17cb19b51224724cc07573defb
Caleb Carlyle <122570866+chcarlyle@users.noreply.github.com>
2024-12-05 09:52:32 -0700

    Update data_cleaning.py

--- nba_2_sample/data_cleaning.py
+++ nba_2_sample/data_cleaning.py
@@ -3,10 +3,10 @@ import pandas as pd
 # Function to clean the scraped data
 def clean_data(df: pd.DataFrame):
     # Example of cleaning: convert columns to appropriate types
-    df['mp'] = pd.to_numeric(df['vpts'], errors='coerce')
-    df['ORtg'] = pd.to_numeric(df['hpts'], errors='coerce')
-    df['DRtg'] = pd.to_numeric(df['hpts'], errors='coerce')
-    df['bpm'] = pd.to_numeric(df['hpts'], errors='coerce')
+    df['mp'] = pd.to_numeric(df['mp'], errors='coerce')
+    df['ORtg'] = pd.to_numeric(df['ORtg'], errors='coerce')
+    df['DRtg'] = pd.to_numeric(df['DRtg'], errors='coerce')
+    df['bpm'] = pd.to_numeric(df['bpm'], errors='coerce')
 
     # Drop rows with missing or invalid data
     df = df.dropna(subset=["mp", "bpm"])
