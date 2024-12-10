74cae0651530168d6efb106b271b0e2f90690e29
Caleb Carlyle <caleb.h.carlyle@gmail.com>
2024-12-10 12:10:21 -0700

    docs and formatting

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -17,21 +14,22 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add the path to your source code
-sys.path.insert(0, os.path.abspath('C:/Users/caleb/OneDrive/Documents/Stat 386/nba_2_sample/nba_2_sample'))  # Update '../src' with the correct relative path to your source code
-
 extensions = [
-    'sphinx.ext.autodoc',           # Auto-generate docs from docstrings
-    'sphinx.ext.napoleon',          # Support Google/NumPy style docstrings
-    'sphinx.ext.autosummary',       # Auto-create summaries for modules
-    'sphinx.ext.viewcode'   # Automatically document type hints
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
+    'myst_parser'
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+source_suffix = ['.rst', '.md']
--- docs/modules.rst
+++ docs/modules.rst
@@ -1,42 +1,7 @@
 nba_2_sample
-===========
+============
 
-.. automodule:: nba_2_sample
-    :members:
-    :undoc-members:
-    :show-inheritance:
+.. toctree::
+   :maxdepth: 4
 
-Submodules
-----------
-
-data_scraping
-=============
-
-.. automodule:: nba_2_sample.data_scraping
-    :members:
-    :undoc-members:
-    :show-inheritance:
-
-data_cleaning
-=============
-
-.. automodule:: nba_2_sample.data_cleaning
-    :members:
-    :undoc-members:
-    :show-inheritance:
-
-analysis
-========
-
-.. automodule:: nba_2_sample.analysis
-    :members:
-    :undoc-members:
-    :show-inheritance:
-
-utils
-=====
-
-.. automodule:: nba_2_sample.utils
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   nba_2_sample
