764e535f1aa94e2441d5528cd3c2a940f4f3560f
Caleb Carlyle <122570866+chcarlyle@users.noreply.github.com>
2024-12-10 10:08:19 -0700

    Changing mp to minutes

--- nba_2_sample/data_cleaning.py
+++ nba_2_sample/data_cleaning.py
@@ -4,10 +4,10 @@ import re
 # Function to clean the scraped data
 def clean_data(df: pd.DataFrame):
     # Example of cleaning: convert columns to appropriate types
-    df['mp'] = pd.to_numeric(df['mp'], errors='coerce')
+    df['mp'] = pd.to_numeric(df['mp'], errors='coerce')/60
     df['ORtg'] = pd.to_numeric(df['ORtg'], errors='coerce')
     df['DRtg'] = pd.to_numeric(df['DRtg'], errors='coerce')
-    df['bpm'] = pd.to_numeric(df['bpm'], errors='coerce')
+    df['bpm'] = pd.to_numeric(df['bpm'], errors='coerce)
 
     # Drop rows with missing or invalid data
     df = df.dropna(subset=["mp", "bpm"])
@@ -16,7 +16,7 @@ def clean_data(df: pd.DataFrame):
 def generate_vorp(df: pd.DataFrame):
     # Calculate VORP: BPM * (Minutes Played / 48) / 8
     # Convert seconds to minutes by dividing by 60
-    df['vorp'] = (df['bpm'] * (df['mp'] / 60) / 48) / 8
+    df['vorp'] = (df['bpm'] * (df['mp']) / 48) / 8
     return df
 
 def generate_net(df: pd.DataFrame):
